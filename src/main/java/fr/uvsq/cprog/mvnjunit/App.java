c9e5286b0530346557b1cc7e3e64ff9d3925653d
uvsq22107773 <abd-elmoumen.drici@ens.uvsq.fr>
2021-10-13 16:57:38 +0200

    la classe et son interface

--- src/main/java/fr/uvsq/cprog/mvnjunit/App.java
+++ src/main/java/fr/uvsq/cprog/mvnjunit/App.java
@@ -6,8 +6,21 @@ package fr.uvsq.cprog.mvnjunit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Fraction fr = new Fraction(12,16);
+        System.out.println(fr.getNum());
+        System.out.println(fr.getDen());
+        System.out.println(fr.toStringg());
+        System.out.println(fr.toDouble());
+
+        System.out.println();
+        System.out.println(fr.add(new Fraction(4,2)).toStringg());
+
+        System.out.println();
+        System.out.println(fr.equals(new Fraction(6,7)));
+
+        System.out.println();
+        System.out.println(fr.compareTo(new Fraction(1,7)));
+
     }
 }
